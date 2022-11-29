61124bc55f6ead37aae32c857f5b342c047e5af3
ARASHFAQUE <55973326+ARASHFAQUE@users.noreply.github.com>
2022-11-29 18:25:20 +0600

    first commit from ashfaque

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:signup_login/view/custom_widget/my_theme.dart';
+import 'package:signup_login/view/welcome_page/welcome_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
+      home: const WelcomePage(),
+    );
+  }
+}
+
+
+
+
